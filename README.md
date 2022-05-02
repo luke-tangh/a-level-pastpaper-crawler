94004ab3451fe9bab45b782dd7680800083fdea2
Luke.Tang <43289451+Clob4k@users.noreply.github.com>
2022-05-02 22:52:40 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # a-level-pastpaper-crawler
 crawl cambridge a level papers from papers.gceguide.com
+
+* change the variables `year` and `subject_code` to suit your needs
+
+* crawl delay are set to minimum value of 30 seconds according to the robots from the website.
+
+# supported subjects
+
+* 9709 mathematics
+* 9702 physics
+* 9701 chemistry
+* 9700 biology
+* 9708 economics
+* 9093 english
+* 9696 geography
+* 9489 history
+* 9715 chinese
+* 9618 computer science
+
+You can add more subjects by adding subjects in the `subject_dict`.
+
+# robots
+```
+User-agent: *
+Crawl-delay: 30
+Disallow: 
+Disallow: /wp-includes/*
+Disallow: /Books/*
+Disallow: /assets/*
+Disallow: /files/*
+Disallow: /draft/*
+Disallow: /wp-content/*
+```
---
+++ pastpaper-downloader.py
@@ -0,0 +1,117 @@
+# %% [markdown]
+# # PASTPAPER DOWNLOADER
+
+# %% [markdown]
+# 1.前置需求 Requirements
+
+# %%
+import requests
+import time
+import os
+import re
+
+# %% [markdown]
+# 2.函数定义 Functions
+
+# %%
+# 爬取延迟设置 Crawl Delay
+def crawl_delay():
+    delay = 30
+    print('pause for crawl delay...({}s)'.format(delay))
+    time.sleep(delay)
+
+# %%
+# 获取网站html get HTML
+def get_html(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r
+    except requests.HTTPError:
+        return "HTTPError"
+
+# %%
+# 获取目录下pdf文件名 Get pdf name
+def get_pdf_name(url, subject_code):
+    web = get_html(url)
+    web_text = web.text
+    pdf_list = re.findall(r'('+str(subject_code)+'.*?.pdf)', web_text)
+    pdf_set = set()
+    print("list of download:")
+    for i in range(len(pdf_list)):
+        if len(pdf_list[i]) < 20:
+            pdf_set.add(pdf_list[i])
+            
+            print(pdf_list[i])
+    return pdf_set
+
+# %% [markdown]
+# 3.参数设置 parameters
+
+# %%
+
+# download information
+year = '2015'
+subject_code = '9608'
+# reconnect times for network errors
+trial_max = 5
+save_path = './{}/{}/'.format(subject_code, year)
+
+subject_dict = {"9702" : "Physics", "9701" : "Chemistry", "9700" : "Biology", \
+                "9696" : "Geography", "9706" : "History", "9489" : "Economics",\
+                "9093" : "English", "9710" : "Chinese", "9715" : "Mathematics",\
+                "9608" : "Computer%20Science%20(for%20final%20examination%20in%202021)",\
+                "9618" : "Computer%20Science%20(for%20first%20examination%20in%202021)"}
+
+subject_name = subject_dict[subject_code]
+subject = '{}%20({})'.format(subject_name,subject_code)
+url = "https://papers.gceguide.com/A%20Levels/{}/{}/".format(subject,year)
+print("target url: {}".format(url))
+
+r = requests.get(url, timeout=30)
+if r.status_code != 200:
+    print('connection failed, double check the parameters')
+else:
+    print('successfully connected')
+
+
+# %% [markdown]
+# 4.主程序 Main program
+
+# %%
+pdf_set = get_pdf_name(url, subject_code)
+count = 1
+trial_max = 5
+for pdf in pdf_set:
+    trial = 1
+    print("=====================")
+    print(pdf)
+    print(count, "/", len(pdf_set))
+    print("=====================")
+    pdf_url = url + pdf
+    pdf_name = save_path + pdf
+    if os.path.exists(save_path) == False:
+        os.makedirs(save_path)
+    if os.path.exists(pdf_name):
+        print("The file has already existed")
+    else:
+        print("Downloading...")
+        pdf_content = get_html(pdf_url)
+        while pdf_content == "HTTPError":
+            crawl_delay()
+            print("Download failed. Retrying...{}/{}".format(trial, trial_max))
+            pdf_content = get_html(pdf_url)
+            if trial == trial_max:
+                print("Download failed. Automatically switch to next item.")
+                continue
+            trial += 1
+        pdf_content = pdf_content.content
+        with open(pdf_name, "wb") as f:
+            f.write(pdf_content)
+        print("successfully downloaded")
+        crawl_delay()
+    count += 1
+print("Download completed {}/{}".format(count-1, len(pdf_set)))
+
+os.system('pause')
