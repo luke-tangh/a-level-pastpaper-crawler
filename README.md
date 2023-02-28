7fc2dc152221f0a7ed377e0542e6af831d224fef
Luke.Tang <Luke.TangH@outlook.com>
2023-02-28 19:04:07 +0800

    code refactoring

--- README.md
+++ README.md
@@ -1,43 +1,35 @@
 # a-level-pastpaper-crawler
 Crawl cambridge a level papers from _papers.gceguide.com_
 
-- Choose from combo boxs to suit your needs
-
 - Amend `subject.json` or `subject_dict` to add additional subjects
 
-- Crawl delay are set to minimum value of 30 seconds according to the robots from the website.
+- Crawl delay is set to minimum value of 30 seconds according to [_robots.txt_](#robots) from the website.
 
-- Papers are saved in current working folder by default, if needed, `save_path` variable may be modified. 
+- Papers are saved in current working folder by default. If needed, `save_path` may be modified. 
 
 # libs
-
 - `requests`
-- `pyqt5` _optional_
+- `pyqt5` _(essential for gui version)_
 
 # run command line version
-
-1. git clone `https://github.com/luke-tangh/a-level-paper-downloader.git` 
-2. open `lite_downloader.py` with editor
-3. modify `subject_code` and `year` below _line 32_
-4. run `lite_downloader.py`
-5. wait for download to complete
+1. git clone `https://github.com/luke-tangh/a-level-paper-downloader.git` or download ``lite_downloader.py``
+2. run `lite_downloader.py`
+3. input `subject` and `year`
+4. wait for download to complete
 
 
-# run gui version
+# run gui version (not recommended)
 1. git clone `https://github.com/luke-tangh/a-level-paper-downloader.git`
 2. run `main.pyw`
 3. modify `Subject`, `Year` and `Type` and click download
 4. wait for download to complete
-
 
 > qp - question paper\
 > ms - mark scheme\
 > gt - grade threshold\
 > ci - confidential information
 
-
 # supported subjects
-
 - 9231 Mathematics Further
 - 9489 History
 - 9608 Computer Science (old)
@@ -51,8 +43,7 @@ Crawl cambridge a level papers from _papers.gceguide.com_
 - 9990 Psychology
 
 # robots
-
-Retrieve from _https://www.gceguide.com/robots.txt_
+Retrieved from _https://www.gceguide.com/robots.txt_
 
 ```
 User-agent: *
--- lite_downloader.py
+++ lite_downloader.py
@@ -23,28 +23,33 @@ def get_html(url):
         return None
 
 
-def get_pdf_name(url, subject_code):
-    web = get_html(url)
-    web_text = web.text
+def get_pdf_name(web_text: str, subject_code: str) -> set[str]:
     pdf_list = re.findall(r'(' + subject_code + '.*?.pdf)', web_text)
     pdf_set = set(pdf_list)
     return pdf_set
 
 
-# parameters
-subject_code = input("enter the subject code(eg. 9702):")
-while len(subject_code) != 4 and not subject_code.isdigit():
-    print("invalid subject code, please retry")
-    subject_code = input("enter the subject code(eg. 9702):")
-
-year = input("enter the year to download(eg. 2015):")
-while len(year) != 4 and not year.isdigit():
-    print("invalid year, please retry")
-    year = input("enter the year to download(eg. 2015):")
+def input_sub_code():
+    ipt_code = input("enter the subject code(eg. 9702):")
+    while len(ipt_code) != 4 and not ipt_code.isdigit():
+        print("invalid subject code, please retry")
+        ipt_code = input("enter the subject code(eg. 9702):")
+    return ipt_code
 
 
-trial_max = 5
-save_path = './{}/{}/'.format(subject_code, year)
+def input_year():
+    ipt_year = input("enter the year to download(eg. 2015):")
+    while len(ipt_year) != 4 and not ipt_year.isdigit():
+        print("invalid year, please retry")
+        ipt_year = input("enter the year to download(eg. 2015):")
+    return ipt_year
+
+
+def print_file_info(count, pdf_set, pdf):
+    print("---------------------")
+    print(pdf)
+    print(count, "/", len(pdf_set))
+    print("---------------------")
 
 
 subject_dict = {
@@ -56,58 +61,78 @@ subject_dict = {
     "9700": "Biology%20(9700)",
     "9701": "Chemistry%20(9701)",
     "9702": "Physics%20(9702)",
-    "9708": "Economics(9708)",
+    "9708": "Economics%20(9708)",
     "9709": "Mathematics%20(9709)",
     "9990": "Psychology%20(9990)"
     }
+
+
+# parameters
+count = 1
+trial_max = 5
+
+subject_code = input_sub_code()
+while subject_code not in subject_dict:
+    print("subject code {} is not supported".format(subject_code))
+    subject_code = input_sub_code()
+ 
+year = input_year()
 
 
 subject = subject_dict[subject_code]
 url = "https://papers.gceguide.com/A%20Levels/{}/{}/".format(subject, year)
 print("target url: {}".format(url))
 
-r = requests.get(url, timeout=30)
+
+r = get_html(url)
 if r.status_code != 200:
     print('connection failed, double check the parameters')
 else:
     print('successfully connected')
 
 
-pdf_set = get_pdf_name(url, subject_code)
+save_path = './{}/{}/'.format(subject_code, year)
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
+
+pdf_set = get_pdf_name(r.text, subject_code)
 print("list of downloading:")
 for pdf in pdf_set:
     print(pdf)
 
-count = 1
-trial_max = 5
+
 for pdf in pdf_set:
     trial = 1
-    print("=====================")
-    print(pdf)
-    print(count, "/", len(pdf_set))
-    print("=====================")
+    print_file_info(count, pdf_set, pdf)
     pdf_url = url + pdf
     pdf_name = save_path + pdf
-    if os.path.exists(save_path) == False:
-        os.makedirs(save_path)
+    
     if os.path.exists(pdf_name):
-        print("The file has already existed")
-    else:
-        print("Downloading...")
-        pdf_content = get_html(pdf_url)
-        while pdf_content is None:
-            crawl_delay()
-            print("Download failed. Retrying...{}/{}".format(trial, trial_max))
-            pdf_content = get_html(pdf_url)
-            if trial == trial_max:
-                print("Download failed. Automatically switch to next item.")
-                continue
-            trial += 1
-        pdf_content = pdf_content.content
-        with open(pdf_name, "wb") as f:
-            f.write(pdf_content)
-        print("successfully downloaded")
+        print("File already existed")
+        count += 1
+        continue
+    
+    print("Downloading...")
+    pdf_download = get_html(pdf_url)
+    while pdf_download is None:
         crawl_delay()
+        print("Download failed. Retrying...{}/{}".format(trial, trial_max))
+        pdf_download = get_html(pdf_url)
+        if trial == trial_max:
+            print("Download failed. Automatically switch to next item.")
+            break
+        trial += 1
+    
+    if pdf_download is None:
+        continue
+
+    pdf_download = pdf_download.content
+    with open(pdf_name, "wb") as f:
+        f.write(pdf_download)
+    print("successfully downloaded")
+    crawl_delay()
     count += 1
+
 print("Download completed {}/{}".format(count-1, len(pdf_set)))
+input("Press enter to exit...")
